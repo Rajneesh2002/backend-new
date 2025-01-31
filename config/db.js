1038bd81eae597638f6da1ef2dc4bdb2db88b7ab
Rajneesh Kushwaha <rajneeshasp29@gmail.com>
2025-01-31 20:55:23 +0530

    small change

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-const url =
-  "mongodb+srv://rajneeshasp29:rajneesh123@cluster0.5umti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
