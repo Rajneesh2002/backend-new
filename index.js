ae1538834adbec8595edbe0df522e6396092abe5
Rajneesh Kushwaha <rajneeshasp29@gmail.com>
2025-02-01 11:28:21 +0530

    dirname added

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.route.js";
 import resourceRoutes from "./routes/resource.route.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
+// Manually define __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 dotenv.config();
-app.use(express.static('public'))
+
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,7 +29,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/resource", resourceRoutes);
 
 app.get("/", (req, res) => {
-  res.render("index.html");
+  res.render("index");
 });
 
 app.listen(port, () => {
